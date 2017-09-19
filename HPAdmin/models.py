eaa423aeb486e4c169ed0d8058d0807fc4b748b3
Benjamin <350105629@qq.com>
2017-09-19 23:32:34 +0800

    Signed-off-by: Benjamin <350105629@qq.com>

--- HPAdmin/models.py
+++ HPAdmin/models.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class IndexBanerInfo(models.Model):
+    img = models.ImageField()
+    a = models.CharField(max_length=100)
+    title = models.CharField(max_length=30)
+    p = models.CharField(max_length=50)
+    status = models.SmallIntegerField(null=True)
+
+class IndexMovieInfo(models.Model):
+    img = models.ImageField()
+    a = models.CharField(max_length=100)
+    title = models.CharField(max_length=40)
--- myapp/settings.py
+++ myapp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'homepage',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
